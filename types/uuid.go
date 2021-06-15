cbeeeee0a990183c2db799cb96ee1c947881f2da
lontten <lontten@163.com>
2021-06-15 15:07:53 +0800

    type null

--- types/uuid.go
+++ types/uuid.go
@@ -50,82 +50,8 @@ func (u *UUID) Scan(v interface{}) error {
 	return fmt.Errorf("can not convert %v to uuid", v)
 }
 
-// NullUUID represents a uuid.UUID that may be null.
-// NullUUID implements the Scanner interface so
-// it can be used as a scan destination, similar to NullString.
-type NullUUID struct {
-	UUID  uuid.UUID
-	Valid bool // Valid is true if UUID is not NULL
-}
-
-// Scan implements the Scanner interface.
-func (u *NullUUID) Scan(v interface{}) error {
-	if v == nil {
-		u.UUID, u.Valid = uuid.UUID{}, false
-		return nil
-	}
-	u.Valid = true
-	value, ok := v.(string)
-	if ok {
-		u.UUID = uuid.FromStringOrNil(value)
-		return nil
-	}
-	return fmt.Errorf("can not convert %v to timestamp", v)
-}
-
-// Value implements the driver Valuer interface.
-func (u NullUUID) Value() (driver.Value, error) {
-	if !u.Valid {
-		return nil, nil
-	}
-	return u.UUID, nil
-}
-
-func (u NullUUID) IsNull() bool {
-	return !u.Valid
-}
-
-func (u NullUUID) MarshalJSON() ([]byte, error) {
-	var str = ""
-	if !u.Valid {
-		return []byte(fmt.Sprintf(`"%s"`, str)), nil
-	}
-	str = strings.ReplaceAll(u.UUID.String(), "-", "")
-	return []byte(fmt.Sprintf(`"%s"`, str)), nil
-
-}
-
-func (u *NullUUID) UnmarshalJSON(src []byte) error {
-	if len(src) != 34 {
-		return errors.Errorf("invalid length for UUID: %v", len(src))
-	}
-	orNil := uuid.FromStringOrNil(string(src[1 : len(src)-1]))
-	u.UUID, u.Valid = orNil, orNil != uuid.Nil
-	return nil
-}
-
-func (u *NullUUID) Set(v string) {
-	orNil := uuid.FromStringOrNil(v)
-	u.UUID, u.Valid = orNil, orNil != uuid.Nil
-}
-
-func (u *NullUUID) SetUUID(v uuid.UUID) {
-	u.UUID, u.Valid = v, v != uuid.Nil
-}
-
-func String2NullUUID(v string) NullUUID {
-	var u NullUUID
-	orNil := uuid.FromStringOrNil(v)
-	u.UUID, u.Valid = orNil, orNil != uuid.Nil
-	return u
-}
-
 func String2UUID(v string) UUID {
 	return UUID{uuid.FromStringOrNil(v)}
-}
-
-func FromUUID(uuid UUID) NullUUID {
-	return NullUUID{UUID: uuid.UUID, Valid: true}
 }
 
 type UUIDList []UUID
