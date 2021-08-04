de7486e3e63393472ae48f9844fd8c4bcc5df16e
lontten <lontten@163.com>
2021-08-04 17:53:55 +0800

    time

--- types/time.go
+++ types/time.go
@@ -26,6 +26,26 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 	return err
 }
 
+// Value insert timestamp into mysql need this function.
+func (t Time) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	if t.Time.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return t.Time, nil
+}
+
+// Scan valueof jstime.Time
+func (t *Time) Scan(v interface{}) error {
+	value, ok := v.(time.Time)
+	if ok {
+		*t = Time{Time: value}
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+
 type TimeList []Time
 
 // gorm 自定义结构需要实现 Value Scan 两个方法
