c621900c90d2ec85f090fedbdca87e7bfece30d3
lontten <lontten@163.com>
2021-08-17 18:12:06 +0800

    uuid

--- types/uuid.go
+++ types/uuid.go
@@ -3,75 +3,15 @@ package types
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"fmt"
 	"github.com/gofrs/uuid"
 	"github.com/jackc/pgtype"
-	"github.com/pkg/errors"
-	"strings"
 )
 
-type UUID struct {
-	uuid.UUID
-}
+type UUIDList []uuid.UUID
 
-func (u UUID) MarshalJSON() ([]byte, error) {
-	all := strings.ReplaceAll(u.String(), "-", "")
-	rs := []byte(fmt.Sprintf(`"%s"`, all))
-
-	return rs, nil
-
-}
-
-func (u *UUID) UnmarshalJSON(src []byte) error {
-	if len(src) != 34 {
-		return errors.Errorf("invalid length for UUID: %v", len(src))
-	}
-	fromString, err := uuid.FromString(string(src[1 : len(src)-1]))
-	if err != nil {
-		return err
-	}
-	*u = UUID{fromString}
-	return err
-}
-
-
-func (u UUID) Value() (driver.Value, error) {
-	return u.UUID.String(), nil
-}
-
-// Scan valueof time.Time
-func (u *UUID) Scan(v interface{}) error {
-	value, ok := v.(string)
-	if ok {
-		*u = UUID{uuid.FromStringOrNil(value)}
-		return nil
-	}
-	return fmt.Errorf("can not convert %v to uuid", v)
-}
-
-func Str2UUIDMust(v string) UUID {
-	return UUID{uuid.FromStringOrNil(v)}
-}
-
-func NewV4() UUID {
-	v4, _ := uuid.NewV4()
-	return UUID{v4}
-}
-
-func Str2UUID(v string) (UUID, error) {
-	id, err := uuid.FromString(v)
-	if err != nil {
-		return UUID{}, err
-	}
-	return UUID{id}, nil
-}
-
-type UUIDList []UUID
-
-// gorm 自定义结构需要实现 Value Scan 两个方法
 // Value 实现方法
 func (p UUIDList) Value() (driver.Value, error) {
-	var k []UUID
+	var k []uuid.UUID
 	k = p
 	marshal, err := json.Marshal(k)
 	if err != nil {
@@ -93,10 +33,10 @@ func (p *UUIDList) Scan(data interface{}) error {
 	if err != nil {
 		return err
 	}
-	var list []UUID
-	list = make([]UUID, len(array.Elements))
+	var list []uuid.UUID
+	list = make([]uuid.UUID, len(array.Elements))
 	for i, element := range array.Elements {
-		list[i] = UUID{element.Bytes}
+		list[i] = element.Bytes
 	}
 	marshal, err := json.Marshal(list)
 	if err != nil {
