a96bf0680bf0f6b2fa0e2bb7b831f9a513eabf8e
lontten <lontten@163.com>
2021-12-07 16:41:04 +0800

    fix test selectByModel

--- engin_table_sel_test.go
+++ engin_table_sel_test.go
@@ -62,31 +62,45 @@ func TestSelectByModel(t *testing.T) {
 
 	engine := MustConnectMock(db, &PgConf{}).Db(nil)
 
-	mock.ExpectExec("SELECT *").
+	mock.ExpectQuery("SELECT *").
 		WithArgs("kk").
 		WillReturnError(nil).
-		WillReturnResult(sqlmock.NewResult(0, 1))
+		WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "test"),
+		)
 
-	mock.ExpectExec("SELECT *").
+	mock.ExpectQuery("SELECT *").
 		WithArgs("kk", 233).
 		WillReturnError(nil).
-		WillReturnResult(sqlmock.NewResult(0, 1))
+		WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).
+			AddRow(1, "test").
+			AddRow(2, "test2"),
+		)
 
-	num, err := engine.Table.Delete(User{}).ByModel(Whe{
+	user := User{}
+	num, err := engine.Table.Select(User{}).ByModel(Whe{
 		Name: types.NewString("kk"),
 		Age:  nil,
 		Uid:  nil,
-	})
+	}).ScanOne(&user)
 	as.Nil(err)
 	as.Equal(int64(1), num)
+	as.Equal(int64(1), user.Id)
+	as.Equal("test", user.Name)
 
-	num, err = engine.Table.Delete(User{}).ByModel(Whe{
+	users := make([]User, 0)
+	num, err = engine.Table.Select(User{}).ByModel(Whe{
 		Name: types.NewString("kk"),
 		Age:  types.NewInt(233),
 		Uid:  nil,
-	})
+	}).ScanList(&users)
 	as.Nil(err)
-	as.Equal(1, num)
+	as.Equal(int64(2), num)
+	as.Equal(2, len(users))
+	as.Equal(int64(1), users[0].Id)
+	as.Equal("test", users[0].Name)
+	as.Equal(int64(2), users[1].Id)
+	as.Equal("test2", users[1].Name)
 
 	as.Nil(mock.ExpectationsWereMet(), "we make sure that all expectations were met")
 }
