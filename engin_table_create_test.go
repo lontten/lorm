cb6914ddc084bd8835edb6e69424b04bb1a33038
lontten <lontten@163.com>
2021-12-08 10:48:11 +0800

    add test create

---
+++ engin_table_create_test.go
@@ -0,0 +1,83 @@
+package lorm
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/lontten/lorm/types"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	as := assert.New(t)
+	db, mock, err := sqlmock.New()
+	as.Nil(err, "new sqlmock error")
+	engine := MustConnectMock(db, &PgConf{}).Db(nil)
+
+	mock.ExpectQuery("user_p *").
+		WithArgs(2, "up").
+		WillReturnError(nil).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(88))
+
+	user := UserP{
+		Name: types.NewString("up"),
+		Id:   types.NewInt(2),
+	}
+	num, err := engine.Table.Create(&user)
+	as.Nil(err)
+	as.Equal(int64(1), num)
+	as.Equal(88, *user.Id)
+
+	as.Nil(mock.ExpectationsWereMet(), "we make sure that all expectations were met")
+}
+
+func _TestCreateOrUpdate(t *testing.T) {
+	as := assert.New(t)
+	db, mock, err := sqlmock.New()
+	as.Nil(err, "new sqlmock error")
+	engine := MustConnectMock(db, &PgConf{}).Db(nil)
+
+	//-----------------create--------------------------
+	mock.ExpectQuery("INSERT *").
+		WithArgs(2, "add", "add").
+		WillReturnError(nil).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).
+			AddRow(1),
+		)
+
+	user := UserP{
+		Name: types.NewString("add"),
+		Id:   types.NewInt(2),
+	}
+	num, err := engine.Table.CreateOrUpdate(&user).ByPrimaryKey()
+	as.Nil(err)
+	as.Equal(int64(1), num)
+	as.Equal(1, *user.Id)
+
+	as.Nil(mock.ExpectationsWereMet(), "we make sure that all expectations were met")
+}
+
+func _TestCreateOrUpdate2(t *testing.T) {
+	as := assert.New(t)
+	db, mock, err := sqlmock.New()
+	as.Nil(err, "new sqlmock error")
+	engine := MustConnectMock(db, &PgConf{}).Db(nil)
+
+	//-----------------update--------------------------
+	mock.ExpectQuery("INSERT*").
+		WithArgs(1, "upd", "upd").
+		WillReturnError(nil).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).
+			AddRow(2),
+		)
+
+	user := UserP{
+		Name: types.NewString("upd"),
+		Id:   types.NewInt(2),
+	}
+	num, err := engine.Table.CreateOrUpdate(&user).ByPrimaryKey()
+	as.Nil(err)
+	as.Equal(int64(2), num)
+	as.Equal(2, *user.Id)
+
+	as.Nil(mock.ExpectationsWereMet(), "we make sure that all expectations were met")
+}
