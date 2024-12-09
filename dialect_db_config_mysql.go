a29beb52cfc13b3acd9046f8eed1d08760af6f5e
lontten <lontten@163.com>
2024-12-09 22:26:17 +0800

    ✨ Mysql添加other配置参数，添加mysql默认配置

--- dialect_db_config_mysql.go
+++ dialect_db_config_mysql.go
@@ -10,6 +10,7 @@ type MysqlConf struct {
 	DbName   string
 	User     string
 	Password string
+	Other    string
 }
 
 func (c MysqlConf) dialect(ctx *ormContext) Dialecter {
@@ -21,6 +22,12 @@ func (c MysqlConf) open() (*sql.DB, error) {
 	dsn := c.User + ":" + c.Password +
 		"@tcp(" + c.Host +
 		":" + c.Port +
-		")/" + c.DbName
+		")/" + c.DbName + "?"
+
+	if c.Other == "" {
+		dsn += "charset=utf8mb4&parseTime=True&loc=Local"
+	} else {
+		dsn += c.Other
+	}
 	return sql.Open("mysql", dsn)
 }
