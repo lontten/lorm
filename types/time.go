dccb878b0c23041c36a4ebfee74c83ab3c59a446
lontten <lontten@163.com>
2024-12-28 19:00:50 +0800

    Revert "空字符串null时间"
    This reverts commit 88500861587dc2ef68dfdcd0b37321aee8b08e79.

--- types/time.go
+++ types/time.go
@@ -19,18 +19,7 @@ func NowTime() Time {
 func NowTimeP() *Time {
 	return &Time{time.Now()}
 }
-func isJsonTimeNull(s string) bool {
-	if s == "" {
-		return true
-	}
-	if s == "\"\"" {
-		return true
-	}
-	if s == "null" {
-		return true
-	}
-	return false
-}
+
 func (t Time) ToString() string {
 	return t.Time.Format(`15:04:05`)
 }
@@ -41,7 +30,10 @@ func (t Time) MarshalJSON() ([]byte, error) {
 }
 
 func (t *Time) UnmarshalJSON(data []byte) error {
-	if isJsonTimeNull(string(data)) {
+	if string(data) == "" {
+		return nil
+	}
+	if string(data) == "null" {
 		return nil
 	}
 	now, err := time.ParseInLocation(`"15:04:05"`, string(data), time.Local)
@@ -149,7 +141,10 @@ func (t Date) MarshalJSON() ([]byte, error) {
 }
 
 func (t *Date) UnmarshalJSON(data []byte) error {
-	if isJsonTimeNull(string(data)) {
+	if string(data) == "" {
+		return nil
+	}
+	if string(data) == "null" {
 		return nil
 	}
 	now, err := time.ParseInLocation(`"2006-01-02"`, string(data), time.Local)
@@ -251,7 +246,10 @@ func (t DateTime) MarshalJSON() ([]byte, error) {
 }
 
 func (t *DateTime) UnmarshalJSON(data []byte) error {
-	if isJsonTimeNull(string(data)) {
+	if string(data) == "" {
+		return nil
+	}
+	if string(data) == "null" {
 		return nil
 	}
 	now, err := time.ParseInLocation(`"2006-01-02 15:04:05"`, string(data), time.Local)
